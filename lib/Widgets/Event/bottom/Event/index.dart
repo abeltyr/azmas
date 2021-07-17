587fea12ccaa5ebf081ba4af9a4db40ff5665b8d
abeltyr <abeltyr@gmail.com>
2021-07-18 00:46:34 +0300

    (feat) update on the count down

--- lib/Widgets/Event/bottom/Event/index.dart
+++ lib/Widgets/Event/bottom/Event/index.dart
@@ -2,7 +2,9 @@ import 'package:azmas/Utils/theme.dart';
 import 'package:azmas/Widgets/Event/Bottom/Event/explore.dart';
 import 'package:azmas/Widgets/Event/Bottom/Event/popular.dart';
 import 'package:azmas/Widgets/Event/Bottom/Event/week.dart';
+import 'package:azmas/Widgets/countDown.dart';
 import 'package:azmas/Widgets/loading/check.dart';
+import 'package:azmas/Widgets/loading/rocket.dart';
 import 'package:azmas/Widgets/loading/small.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -41,57 +43,104 @@ class _EventsState extends State<Events> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height,
-      child: Column(
-        children: [
-          SizedBox(
-            height: 100,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height - 100,
+      color: Colors.transparent,
+      child: SmartRefresher(
+          enablePullDown: true,
+          header: WaterDropHeader(
+            completeDuration: Duration(milliseconds: 1500),
+            complete: Container(
+                child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(height: 30, width: 30, child: CheckLoading()),
+                Text(
+                  "Fetched All The Events",
+                  style: GoogleFonts.lora(
+                    color: PlatformTheme.textColor2,
+                    fontWeight: FontWeight.w800,
+                    fontSize: 14,
+                    wordSpacing: 0.5,
+                  ),
+                ),
+              ],
+            )),
+            refresh: SmallLoading(),
+            waterDropColor: PlatformTheme.textColor2,
           ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height - 100,
-            color: Colors.transparent,
-            child: SmartRefresher(
-              enablePullDown: true,
-              header: WaterDropHeader(
-                completeDuration: Duration(milliseconds: 1500),
-                complete: Container(
-                    child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(height: 30, width: 30, child: CheckLoading()),
-                    Text(
-                      "Fetched All The Events",
-                      style: GoogleFonts.lora(
-                        color: PlatformTheme.textColor2,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 14,
-                        wordSpacing: 0.5,
-                      ),
+          controller: _refreshController,
+          onRefresh: _onRefresh,
+          onLoading: _onLoading,
+          child: ListView.builder(
+              itemCount: 1,
+              itemBuilder: (ctx, index) {
+                if (index == 0)
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                    height: 250,
+                    decoration: BoxDecoration(
+                      color: PlatformTheme.white,
+                      borderRadius: BorderRadius.circular(15),
                     ),
-                  ],
-                )),
-                refresh: SmallLoading(),
-                waterDropColor: PlatformTheme.textColor2,
-              ),
-              controller: _refreshController,
-              onRefresh: _onRefresh,
-              onLoading: _onLoading,
-              child: ListView(
-                padding: EdgeInsets.zero,
-                children: [
-                  PopularEventCards(),
-                  SizedBox(height: 15),
-                  WeekEventCards(),
-                  SizedBox(height: 10),
-                  ExploreEventCards(),
-                  SizedBox(height: 90),
-                ],
-              ),
-            ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 15,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.only(
+                            top: 5,
+                          ),
+                          margin: EdgeInsets.only(top: 10),
+                          child: Text(
+                            "Graduation Day Count Down",
+                            style: GoogleFonts.lora(
+                              color: PlatformTheme.textColor1,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 20,
+                              wordSpacing: 1,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Expanded(child: Center(child: Rocket())),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        CountDownWidget(
+                          date: "2021-10-17",
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                      ],
+                    ),
+                  );
+                else
+                  return Container();
+              })
+          // ListView(
+          //   padding: EdgeInsets.zero,
+          //   children: [
+          // PopularEventCards(),
+          // SizedBox(height: 15),
+          // WeekEventCards(),
+          // SizedBox(height: 10),
+          // ExploreEventCards(),
+          // SizedBox(height: 90),
+          // ],
+          // ),
           ),
-        ],
-      ),
+
+      // Container(
+      //   width: MediaQuery.of(context).size.width,
+      //   height: MediaQuery.of(context).size.height - 200,
+      //   color: Colors.transparent,
+      // ),
     );
   }
 }
